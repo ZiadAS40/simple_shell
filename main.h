322da2ba25deca8f8dfe23fabcfd5bc0395a7c84
ZiadAS40 <zead4036@gmail.com>
2024-02-28 16:36:36 +0200

    fix some issues

--- main.h
+++ main.h
@@ -9,7 +9,7 @@
 
 extern char **environ;
 
-int handleChild(char *line,char **arg, char **argv);
+int handleChild(char *line, char **arg, char **argv);
 ssize_t handleRead(char **line, char ***arg);
 void handlePath(char ***arg);
 ssize_t _getline(char **lineptr, size_t *len, FILE *stream);
